6a946a043922bab7210840c16cfac6e65b62380d
Sumit Kumar <ksumit1454@gmail.com>
2026-08-10 20:26:31 +0530

    input and output

---
+++ inputoutput.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x , y ;
+    cout <<"enter the value of x: " << endl ;
+    cin >> x ;
+    cout <<"enter the value of y: " << endl;
+     cin >> y;
+    cout <<"value of x: " << x <<" and y: "<< y;
+    return 0;
+}
