f26436e76ca9053394837b28360fc2893275898d
Sumit Kumar <ksumit1454@gmail.com>
2026-08-12 21:44:04 +0530

    datatype

---
+++ datatype.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // ...........datatype...............
+    // int ,float ,double , long double ,
+    //    string and getline
+
+    // string s1 , s2;
+    // cout <<" enter s1:" <<endl;
+    // cin>> s1 ;
+    // cout <<" value of s2:" <<endl;
+    // cin >> s2;
+    // cout<< s1<< " " << s2 ;
+     
+
+    // char ch;
+    // cout <<" enter the value :"<<endl;
+    // cin >> ch;
+    // cout << ch;
+
+    string str;
+    cout <<" enter the value str:"<< endl;
+    getline (cin,str);
+    cout << str;
+
+    return 0;
+}
