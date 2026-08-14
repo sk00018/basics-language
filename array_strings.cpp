e0ce33b5c66620311772a1e8146a36c929e5f4c5
Sumit Kumar <ksumit1454@gmail.com>
2026-08-14 13:58:09 +0530

    string

--- array_strings.cpp
+++ array_strings.cpp
@@ -21,25 +21,38 @@ int main() {
 
 
 
-  int arr[3][5];
-    // ..........2D ARRAY..............
-    // Taking input from user
-    cout << "Enter 15 numbers:" << endl;
+//   int arr[3][5];
+//     // ..........2D ARRAY..............
+//     // Taking input from user
+//     cout << "Enter 15 numbers:" << endl;
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 5; j++) {
-            cin >> arr[i][j];
-        }
-    }
+//     for (int i = 0; i < 3; i++) {
+//         for (int j = 0; j < 5; j++) {
+//             cin >> arr[i][j];
+//         }
+//     }
 
-    // Displaying the 2D array
-    cout << "The 2D array is:" << endl;
+//     // Displaying the 2D array
+//     cout << "The 2D array is:" << endl;
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 5; j++) {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
+//     for (int i = 0; i < 3; i++) {
+//         for (int j = 0; j < 5; j++) {
+//             cout << arr[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+
+
+
+    string name;
+
+    cout << "Enter your name: ";
+    getline(cin, name);
+
+    cout << "Hello, " << name << "!" << endl;
+
+
     return 0;
 }
+
+
