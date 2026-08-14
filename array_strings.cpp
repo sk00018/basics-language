9e08cf7838e0534199f61158f3859caa586d83cb
Sumit Kumar <ksumit1454@gmail.com>
2026-08-14 13:17:18 +0530

    2D array

--- array_strings.cpp
+++ array_strings.cpp
@@ -1,25 +1,45 @@
 
-
-
 #include <iostream>
 using namespace std;
-
 int main() {
-    // ........1D ARRAY..........
-    int arr[5];
 
-    // Input
-    cout << "Enter 5 numbers: ";
-    for (int i = 0; i < 5; i++) {
-        cin >> arr[i];
+    
+//     // ........1D ARRAY..........
+//     int arr[5];
+
+//     // Input
+//     cout << "Enter 5 numbers: ";
+//     for (int i = 0; i < 5; i++) {
+//         cin >> arr[i];
+//     }
+
+//     // Output
+//     cout << "The numbers are: ";
+//     for (int i = 0; i < 5; i++) {
+//         cout << arr[i] << " ";
+//     }
+
+
+
+  int arr[3][5];
+    // ..........2D ARRAY..............
+    // Taking input from user
+    cout << "Enter 15 numbers:" << endl;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> arr[i][j];
+        }
     }
 
-    // Output
-    cout << "The numbers are: ";
-    for (int i = 0; i < 5; i++) {
-        cout << arr[i] << " ";
+    // Displaying the 2D array
+    cout << "The 2D array is:" << endl;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 5; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
-
-
     return 0;
 }
