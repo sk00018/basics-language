9f435ea172994aa8390edcfba9ae6803bcad5306
Sumit Kumar <ksumit1454@gmail.com>
2026-08-14 15:32:31 +0530

    while

--- loop.cpp
+++ loop.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    for (int i = 0; i <= 10; i++) {
-        cout << "sumit" << i << endl;
-    }
+
+//     for (int i = 0; i <= 10; i++) {
+//         cout << "sumit" << i << endl;
+//     }
+
+
+int i = 1;
+while (i<=5) {
+   cout << "shree krishna:" << i << endl;
+   i = i + 1;
+}
 
     return 0;
 }
