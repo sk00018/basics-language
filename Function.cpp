d7941685de3e74da6f6c846f08f9d39553a916a6
Sumit Kumar <ksumit1454@gmail.com>
2026-08-15 23:22:27 +0530

    calculator using function

--- Function.cpp
+++ Function.cpp
@@ -8,14 +8,29 @@ using namespace std;
 // parameterized
 // non parameterized
 
-void printname(string name){
-cout <<"hey " << name <<endl;
-}
-int main(){
-    string name;
-    cin >> name;
-    printname(name);
+// void printname(string name){
+// cout <<"hey " << name <<endl;
+// }
+// int main(){
+//     string name;
+//     cin >> name;
+//     printname(name);
 
     
+
+    // ......take two numbers and print its sum.........
+//....... like a calculater........ 
+int sum(int num1,int num2){
+    int num3 =  num1 + num2;
+    return num3;
+}
+
+int main(){
+int num1,num2;
+cin >>  num1 >> num2;
+int res = sum(num1, num2);
+cout << res;
+
+
     return 0;
 }
