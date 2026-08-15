ea4ca72710a25672439e77e64518fec64dea36c3
Sumit Kumar <ksumit1454@gmail.com>
2026-08-15 23:24:42 +0530

    pass by value function

--- Function.cpp
+++ Function.cpp
@@ -18,18 +18,34 @@ using namespace std;
 
     
 
-    // ......take two numbers and print its sum.........
-//....... like a calculater........ 
-int sum(int num1,int num2){
-    int num3 =  num1 + num2;
-    return num3;
-}
+//     // ......take two numbers and print its sum.........
+// //....... like a calculater........ 
+// int sum(int num1,int num2){
+//     int num3 =  num1 + num2;
+//     return num3;
+// }
 
+// int main(){
+// int num1,num2;
+// cin >>  num1 >> num2;
+// int res = sum(num1, num2);
+// cout << res;
+
+
+
+// .......pass by value.........
+void dosomthing(int num){
+    cout << num << endl;
+    num +=5;
+    cout << num << endl;
+    num +=5;
+    cout << num << endl;
+    
+}
 int main(){
-int num1,num2;
-cin >>  num1 >> num2;
-int res = sum(num1, num2);
-cout << res;
+    int num = 10;
+    dosomthing(num);
+    cout << num << endl;
 
 
     return 0;
