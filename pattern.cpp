86d68817a4b2fc294ff1bc76e149f76ca70702a9
Sumit Kumar <ksumit1454@gmail.com>
2026-08-23 01:30:54 +0530

    Increasing triangle starpattern

--- pattern.cpp
+++ pattern.cpp
@@ -2,14 +2,29 @@
 using namespace std;
 
 int main(){
-    int n = 5;
 
-    for(int i = 1; i <=n; i++){
-       for(int j = 1; j <=n; j++){ 
-          cout << "*";
+// Pattern 1: square_star_pattern   
+//     int n = 5;
+//     for(int i = 1; i <=n; i++){
+//        for(int j = 1; j <=n; j++){ 
+//           cout << "*";
+//     }
+//     cout << endl;
+// }
+
+
+
+// Pattern 1: Increasing triangle star pattern
+
+    cout << "Pattern 2:\n";
+
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
     }
-    cout << endl;
-}
+
 return 0;
 }
 
