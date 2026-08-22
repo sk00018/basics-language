7478b6815b0ef49c00917ac95b376b3735fe203f
Sumit Kumar <ksumit1454@gmail.com>
2026-08-23 01:00:50 +0530

    square_star_pattern

--- loop.cpp
+++ loop.cpp
@@ -7,21 +7,21 @@ int main() {
 //     }
 
 
-// int i = 1;
-// while (i<=5) {
-//    cout << "shree krishna:" << i << endl;
-//    i = i + 1;
-// }
-
-
-
 int i = 1;
-do
-{
+while (i<=5) {
    cout << "shree krishna:" << i << endl;
    i = i + 1;
-} while (i<=2);
-  cout << i << endl;
+}
+
+
+
+// int i = 1;
+// do
+// {
+//    cout << "shree krishna:" << i << endl;
+//    i = i + 1;
+// } while (i<=2);
+//   // cout << i << endl;
 
     return 0;
 }
